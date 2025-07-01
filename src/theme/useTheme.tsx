b247018c26d91f357233cc303aee3f85083a8a84
Ivan <i.yakovlev@neovoxtech.ru>
2025-07-01 16:41:19 +0300

    removing excess folder

---
+++ src/theme/useTheme.tsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { ETheme, LOCAL_STORAGE_THEME_KEY, ThemeContext } from "./ThemeContext";
+
+interface UseThemeResult {
+  toggleTheme: () => void;
+  theme: ETheme;
+}
+
+export function useTheme(): UseThemeResult {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+    const newTheme = theme === ETheme.Dark ? ETheme.Light : ETheme.Dark;
+    setTheme(newTheme);
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+  };
+
+  return {
+    theme,
+    toggleTheme
+  };
+}
